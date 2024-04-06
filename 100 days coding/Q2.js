d7f2df87432996575abfb69cb4add4e944cdc667
MariumTabbasum20 <160420840+MariumTabbasum20@users.noreply.github.com>
2024-04-07 02:09:48 +0500

    Add files via upload

---
+++ 100 days coding/Q2.js
@@ -0,0 +1,3 @@
+//Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+var Name = 'MARIUM'; //This save the name.
+console.log("Hello ".concat(Name, ",would you like to learn some TypeScript today?")); //This saves the message.
